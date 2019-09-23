0a884ff802d8751240ed4aef932d20c45b8e75f6
aldidefril556 <53865241+aldidefril556@users.noreply.github.com>
2019-09-23 23:59:41 +0700

    canggih123.sh

--- canggih123.sh
+++ canggih123.sh
@@ -10,7 +10,7 @@ ku='\033[33;1m' #kuning
 # Jangan Recode Ya gayn
 echo
 echo
-sh passwoord.sh   
+sh hash.sh   
 echo                                                                  
 clear            
 echo                                                                       

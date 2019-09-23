68b2d5b71d70b53cb801c84ccb775d1012eadfad
aldidefril556 <53865241+aldidefril556@users.noreply.github.com>
2019-09-23 23:20:32 +0700

    canggih123.sh

---
+++ canggih123.sh
@@ -0,0 +1,485 @@
+#usr/bin/sh
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Jangan Recode Ya gayn
+echo
+echo
+sh passwoord.sh   
+echo                                                                  
+clear            
+echo                                                                       
+echo $red"          Tool akan di install dalam waktu 4 detik"
+echo $red"            harap sabar menunggu yaa "
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                            _|_|_ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 3 detik"
+echo $red"            harap sabar menunggu yaa "
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 2 detik"
+echo $red"            harap sabar menunggu yaa "
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 1 detik"
+echo $red"            harap sabar menunggu yaa "
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"         Tool akan di install dalam waktu 5 detik"                            echo $red"            harap sabar menunggu yaa "
+sleep 1                                                                                 echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+# yellow= \033[33;1m
+# indigo= \033[34;1m
+# red= \033[31;1m
+# purple= \033[37;1m
+# cyan= \033[36;1m
+# white= \033[39;1m
+z() {
+echo "
+$i░░█░█▌█▀▀█ █▀▀█ █▀▀█ █▌▄█░░
+░░█▀█▌█▄▄█ █▄▄█ █▄▄█ █▄██░░
+░░█░█▌█▌▐█ █▌░░ █▌░░ ░██░░░
+░$ku╔╦═╦╗───╔╗──────╔╗─$me▄▄─▄▄▀▀▄▀▀▄
+░$ku║║║║╠═╦═╣╠╦═╦═╦╦╝║$me███████───▄▀
+ $ku║║║║║╩╣╩╣═╣╩╣║║║╬║$me▀█████▀▀▄▀
+░$ku╚═╩═╩═╩═╩╩╩═╩╩═╩═╝──$me▀█▀ ♥♥
+        $pur◕▬▬▬▬▬▬▬❋[♥]❋▬▬▬▬▬▬◕
+             $pu █████████ 
+              █▄█████▄█ 
+              █$me ▼▼▼▼▼$pu █ 
+            $pu ██▌    $pu ██▌ 
+             $pu █$me ▲▲▲▲▲$pu █ 
+              █████████ 
+              ██     ██
+ $i▂▃▅▇█▓▒░۩۞۩$me╔══╗$cy╔═╗$i╔═╗$ku╔╗$bi─╔══╗$pur╔╗╔╗$i۩۞۩░▒▓█▇▅▃▂
+            $me╚╗╔╝$cy║║║$i║║║$ku║║$bi─║══╣$pur╚╗╔╝
+            $me ║║ $cy║║║$i║║║$ku║╚╗$bi╠══║$pur╔╝╚╗
+            $me ╚╝ $cy╚═╝$i╚═╝$ku╚═╝$bi╚══╝$pur╚╝╚╝
+
+"
+}
+z
+echo
+echo $pur"==================="$cy" ============================="
+echo $i"Ôûê"$pu"  AUTHOR    :"$me" ALDI DEFRIL"$i"             Ôûê"
+echo $i"Ôûê"$pu"  FACEBOOK  :"$cy" ALDI DEFRIL"$i"             Ôûê"
+echo $i"Ôûê"$pu"  WhatsApp  :"$i" 081268707053"$i"            Ôûê"
+echo $i"Ôûê"$pu"  GMAIL     :"$ku" aldidefril11@gmail.com"$i"  Ôûê"
+echo $i"Ôûê"$pu"  INSTAGRAM :"$bi" @aldidefril12"$i"           Ôûê"
+echo $pur"==================="$cy" ============================="
+echo
+echo $ku"MeNu HaCkInG NeWS:"
+echo $ku"["$me"01"$ku"]"$cy" REPORT FB"
+echo $ku"["$me"02"$ku"]"$cy" BRUTE PORCE"
+echo $ku"["$me"03"$ku"]"$cy" HACK CCTV"
+echo $ku"["$me"04"$ku"]"$cy" MBF"
+echo $ku"["$me"05"$ku"]"$cy" CLONING YAHOO"
+echo $ku"["$me"06"$ku"]"$cy" OSIF"
+echo $ku"["$me"07"$ku"]"$cy" PROFILEGUARD FB"
+echo $ku"["$me"08"$ku"]"$cy" BOT REACTION FACEBOOK"
+echo $ku"["$me"09"$ku"]"$cy" SPAM CALL"
+echo $ku"["$me"10"$ku"]"$cy" SPAM WHATSHAPP"
+echo $ku"["$me"11"$ku"]"$cy" YOUTUBE-DL"
+echo $ku"["$me"12"$ku"]"$cy" LACAK LOKASI"
+echo $ku"["$me"13"$ku"]"$cy" SPAM SMS BRUTAL TERBARU"
+echo $ku"["$me"14"$ku"]"$cy" DARK FB"
+echo $ku"["$me"15"$ku"]"$cy" HACK PULSA"
+echo $ku"["$me"16"$ku"]"$cy" HACK KUOTA 3"
+echo $ku"["$me"17"$ku"]"$cy" NUYUL GoPay"
+echo $ku"["$me"18"$ku"]"$cy" HACK INSTAGRAM"
+echo $ku"["$me"19"$ku"]"$cy" ROOT TERMUX"                                                
+echo $ku"["$me"20"$ku"]"$cy" BIKIN SCRIPT DEFACE"
+echo $ku"["$me"21"$ku"]"$cy" NIK KK KTP GRATIS"                                          
+echo $ku"["$me"22"$ku"]"$cy" HACK LAMPU"
+echo $ku"["$me"23"$ku"]"$cy" LITESPAM"
+echo $ku"["$me"24"$ku"]"$cy" SANTET ONLINE"
+echo $ku"["$me"25"$ku"]"$cy" HACK FACEBOOK TARGET"
+echo $ku"["$me"26"$ku"]"$cy" AUTO LIKE FACEBOOK"
+echo $ku"["$me"27"$ku"]"$cy" PHISHING SECURITY"
+echo $ku"["$me"28"$ku"]"$cy" SADAP WHATSAPP"
+echo $ku"["$me"29"$ku"]"$cy" CLONING FB FOLLOWERS"
+echo $ku"["$me"30"$ku"]"$cy" HACK KAMERA PENIPU"
+echo $i"["$me"00"$i"]"$pu" EXIT"
+echo
+echo $pur"["$pur"Masukkan pilihan anda"$me"]"
+read -p "pilih nomor:" pil
+ 
+if [ $pil = 01 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/IlayTamvan/Report.git
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+ 
+if [ $pil = 02 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Senitopeng/fbbrute.git
+cd fbbrute
+python2 jomblo.py
+fi
+ 
+if [ $pil = 03 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/kancotdiq/ipcs
+cd ipcs
+python2 scan.py
+fi
+ 
+if [ $pil = 04 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/tikuskecil/multi-bruteforce-facebook.git
+cd multi-bruteforce-facebook
+python2 MBF.py
+fi
+ 
+if [ $pil = 05 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/wahyuandhika/YahooCloning.git
+cd YahooCloning
+pip2 install requests mechanize
+pip2 install requirements
+python2 cloning.py
+fi
+ 
+if [ $pil = 06 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/CiKu370/OSIF.git
+cd OSIF
+pip2 install -r requirements.txt
+python2 osif.py
+fi
+ 
+if [ $pil = 07 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php guard.php
+fi
+ 
+if [ $pil = 08 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/edi-nasa12/ROBOT.git
+cd ROBOT
+python2 blackbot.py
+fi
+ 
+if [ $pil = 09 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Aditya021/SpamCall
+cd SpamCall
+php SpamCall.php
+fi
+ 
+if [ $pil = 10 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM.git
+cd LITESPAM
+sh LITESPAM.sh
+fi
+ 
+if [ $pil = 11 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/rahmadxyz/sub-bot.git
+cd sub-bot
+php sub-bot.php
+fi
+ 
+if [ $pil = 12 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator.git
+cd locator
+bash locator.sh
+fi
+ 
+if [ $pil = 13 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/TERMUXID3/brutal-sms
+cd brutal-sms
+python2 run.py
+fi
+ 
+if [ $pil = 14 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/dizart-y/DARK-FB
+cd DARK-FB
+python2 DarkPremium79.py
+fi
+ 
+if [ $pil = 15 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/InYourG00D1/PulsaFree
+cd PulsaFree
+sh Free.sh
+fi
+ 
+if [ $pil = 16 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/InYourG00D1/KuotaFree
+cd KuotaFree
+sh 2.sh
+fi
+ 
+if [ $pil = 17 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/InYourG00D1/Nuyul-GoPay
+cd Nuyul-GoPay
+php Go-Pay.php
+fi
+ 
+if [ $pil = 18 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/linuxkukeren/instagram
+cd instagram
+python2 linuxkerenhacking.py
+fi
+ 
+if [ $pil = 19 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/instainsane
+cd instainsane
+./instainsane.sh
+fi
+ 
+if [ $pil = 20 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Ubaii/script-deface-creator
+cd script-deface-creator
+python2 create.py
+fi
+ 
+if [ $pil = 21 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/zerosvn/ktpkkgenerate
+cd ktpkkgenerate
+php zerosvn.php
+fi
+ 
+if [ $pil = 22 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/LionSec/katoolin.git
+cd katoolin
+python2 katooli.py
+fi
+ 
+if [ $pil = 23 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/4L13199/LITESPAM
+cd LITESPAM
+sh LITESPAM.sh
+fi
+ 
+if [ $pil = 24 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Gameye98/santet-online
+cd santet-online
+python2 santet.py
+fi
+ 
+if [ $pil = 25 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/aldidefril556/aldi6
+cd aldi6
+python2 brute.py
+fi
+ 
+if [ $pil = 26 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/FR13ND8/autolike
+cd autolike
+php autolike.php
+fi
+ 
+if [ $pil = 27 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/evait-security/weeman.git
+cd weeman
+python2 weeman.py
+fi
+ 
+if [ $pil = 28 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/Bl4ckDr460n/HxWhatsApp
+cd HxWhatsApp
+python2 HxWhatsApp.py
+fi
+ 
+if [ $pil = 29 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/DulLah-SL/yahoo-checker
+cd yahoo-checker
+python2 yahoo.py
+fi
+ 
+if [ $pil = 30 ]
+then
+clear
+figlet -f slant "W A I T"|lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+ 
+if [ $pil = 00 ]
+then
+clear
+figlet -f slant "W A I T"|Ulolcat
+sleep 2
+echo $cy"Terima Kasih sudah mengunakan Tools ini"
+sleep 2
+echo $i"Silahkan di pakai semoga bermanfaat Buat anda"
+sleep 2
+echo $pur"Bila Ada Kesalahan Anda Bisa Nanya Melalui Via"
+sleep 2
+echo $ku"Facebook :"$i" Aldi Defril"
+echo $ku"WhatsApp :"$i" 081268707053"
+sleep 2
+echo $pur"Terima kasih Yang Sudah Support dengan Tools ini"
+sleep 2
+echo $pur"SUBSCRIBE MY CHANNEL { YOUTUBE RMD }"
+exit
+fi
+
